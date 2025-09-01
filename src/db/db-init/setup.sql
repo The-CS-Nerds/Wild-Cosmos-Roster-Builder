0ee1ef91e75d65f33ed16510b9cd38381100f80c
SuitablyMysterious <196222145+SuitablyMysterious@users.noreply.github.com>
2025-09-01 12:03:29 +0100

    create a setup sql script

---
+++ src/db/db-init/setup.sql
@@ -0,0 +1,12 @@
+create schema if not exists gamedata;
+create schema if not exists userdata;
+create schema if not exists appdata;
+
+create table if not exists userdata.users (
+    id bigserial primary key;
+    username text not null;
+    password varchar (255) not null;
+    timeCreated timestamptz not null default now()
+)
+
+create index on userdata.users (username, timeCreated);
