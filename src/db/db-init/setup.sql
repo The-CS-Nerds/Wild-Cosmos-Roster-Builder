4de773039486ac588fced934644408375abe1fe1
SuitablyMysterious <196222145+SuitablyMysterious@users.noreply.github.com>
2025-09-01 12:20:16 +0100

    change semi-colons to commas

--- src/db/db-init/setup.sql
+++ src/db/db-init/setup.sql
@@ -3,9 +3,9 @@ create schema if not exists userdata;
 create schema if not exists appdata;
 
 create table if not exists userdata.users (
-    id bigserial primary key;
-    username text not null;
-    password varchar (255) not null;
+    id bigserial primary key,
+    username text not null,
+    password varchar (255) not null,
     timeCreated timestamptz not null default now()
 )
 
